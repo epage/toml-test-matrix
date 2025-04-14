70e87c0e9064e96a7f0fad8cc54029e7e5b7e285
Ed Page <eopage@gmail.com>
2025-04-14 11:52:13 -0500

    Upgrade boml to 1.0.1

--- scripts/rust-boml/src/decoder.rs
+++ scripts/rust-boml/src/decoder.rs
@@ -10,7 +10,7 @@ impl toml_test_harness::Decoder for Decoder {
         let data = std::str::from_utf8(data).map_err(toml_test_harness::Error::new)?;
         let table = boml::Toml::parse(&data)
             .map_err(|err| toml_test_harness::Error::new(format!("{err:?}")))?
-            .into_table();
+            .into();
         table_to_decoded(&table)
     }
 }
@@ -28,22 +28,22 @@ fn value_to_decoded(
         boml::types::TomlValue::Float(v) => Ok(toml_test_harness::Decoded::Value(
             toml_test_harness::DecodedValue::from(*v),
         )),
-        boml::types::TomlValue::OffsetDateTime => Ok(toml_test_harness::Decoded::Value(
-            toml_test_harness::DecodedValue::from("OffsetDateTime"),
+        boml::types::TomlValue::OffsetDateTime(v) => Ok(toml_test_harness::Decoded::Value(
+            toml_test_harness::DecodedValue::Datetime(render_offsetdatetime(v)),
         )),
-        boml::types::TomlValue::LocalDateTime => Ok(toml_test_harness::Decoded::Value(
-            toml_test_harness::DecodedValue::from("LocalDateTime"),
+        boml::types::TomlValue::DateTime(v) => Ok(toml_test_harness::Decoded::Value(
+            toml_test_harness::DecodedValue::DatetimeLocal(render_datetime(v)),
         )),
-        boml::types::TomlValue::LocalDate => Ok(toml_test_harness::Decoded::Value(
-            toml_test_harness::DecodedValue::from("LocalDate"),
+        boml::types::TomlValue::Date(v) => Ok(toml_test_harness::Decoded::Value(
+            toml_test_harness::DecodedValue::DateLocal(render_date(v)),
         )),
-        boml::types::TomlValue::LocalTime => Ok(toml_test_harness::Decoded::Value(
-            toml_test_harness::DecodedValue::from("LocalTime"),
+        boml::types::TomlValue::Time(v) => Ok(toml_test_harness::Decoded::Value(
+            toml_test_harness::DecodedValue::TimeLocal(render_time(v)),
         )),
         boml::types::TomlValue::Boolean(v) => Ok(toml_test_harness::Decoded::Value(
             toml_test_harness::DecodedValue::from(*v),
         )),
-        boml::types::TomlValue::Array(v) => {
+        boml::types::TomlValue::Array(v, _) => {
             let v: Result<_, toml_test_harness::Error> = v.iter().map(value_to_decoded).collect();
             Ok(toml_test_harness::Decoded::Array(v?))
         }
@@ -52,15 +52,44 @@ fn value_to_decoded(
 }
 
 fn table_to_decoded(
-    value: &boml::table::Table,
+    value: &boml::table::TomlTable,
 ) -> Result<toml_test_harness::Decoded, toml_test_harness::Error> {
     let table: Result<_, toml_test_harness::Error> = value
         .iter()
         .map(|(k, v)| {
-            let k = k.to_owned();
+            let k = k.as_str().to_owned();
             let v = value_to_decoded(v)?;
             Ok((k, v))
         })
         .collect();
     Ok(toml_test_harness::Decoded::Table(table?))
+}
+
+fn render_offsetdatetime(value: &boml::types::OffsetTomlDateTime) -> String {
+    format!(
+        "{}t{}{}",
+        render_date(&value.date),
+        render_time(&value.time),
+        render_offset(&value.offset)
+    )
+}
+
+fn render_datetime(value: &boml::types::TomlDateTime) -> String {
+    format!("{}t{}", render_date(&value.date), render_time(&value.time))
+}
+
+fn render_date(value: &boml::types::TomlDate) -> String {
+    format!("{}-{}-{}", value.year, value.month, value.month_day)
+}
+
+fn render_time(value: &boml::types::TomlTime) -> String {
+    format!(
+        "{:2}:{:2}:{:2}.{}",
+        value.hour, value.minute, value.second, value.nanosecond
+    )
+}
+
+fn render_offset(value: &boml::types::TomlOffset) -> String {
+    let prefix = if value.hour < 0 { "-" } else { "+" };
+    format!("{prefix}{:2}:{:2}", value.hour.abs(), value.minute)
 }
